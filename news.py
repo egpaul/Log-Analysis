92deaafd17341427ba17f891f341d496b0079498
Eric Paul <eric.paul@me.com>
2018-01-05 16:02:16 -0500

    Added Python file

---
+++ news.py
@@ -0,0 +1,8 @@
+import psycopg2
+DB_Name = "news"
+
+# 1. What are the three most popular articles of all time?
+
+# 2. Who are the most popular article authors of all time?
+
+# 3. On whih days did more than 1% of requests lead to errors?
